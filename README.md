9c7c75e1fedc803661ac79fb57f49b8e4e1607a3
sahandbabali <sahand.babali@gmail.com>
2021-11-05 00:36:37 +0330

    locking functionality added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 To do:
 
-- disable inactive services from select
-- add locking functionality
+- disable inactive services from select - done
+- add locking functionality - done
 - add data and functionality for other ervices
 - add footer
 - a modal or alert for guide
--- app.js
+++ app.js
@@ -68,13 +68,71 @@ let template1 = document.getElementById("temp1");
 let template2 = document.getElementById("temp2");
 let template3 = document.getElementById("temp3");
 
+let lock1 = false;
+let lock2 = false;
+let lock3 = false;
+
 document.getElementById("newideabutton").addEventListener("click", newidea);
 
+// locking functionality
+
+document.getElementById("lock1").addEventListener("click", function () {
+  if (lock1 == false) {
+    lock1 = true;
+    document.getElementById("lock1").classList.add("fa-lock");
+
+    document.getElementById("lock1").classList.remove("fa-lock-open");
+  } else if (lock1 == true) {
+    lock1 = false;
+    document.getElementById("lock1").classList.add("fa-lock-open");
+
+    document.getElementById("lock1").classList.remove("fa-lock");
+  }
+});
+
+document.getElementById("lock2").addEventListener("click", function () {
+  if (lock2 == false) {
+    lock2 = true;
+    document.getElementById("lock2").classList.add("fa-lock");
+
+    document.getElementById("lock2").classList.remove("fa-lock-open");
+  } else if (lock2 == true) {
+    lock2 = false;
+    document.getElementById("lock2").classList.add("fa-lock-open");
+
+    document.getElementById("lock2").classList.remove("fa-lock");
+  }
+});
+
+document.getElementById("lock3").addEventListener("click", function () {
+  if (lock3 == false) {
+    lock3 = true;
+    document.getElementById("lock3").classList.add("fa-lock");
+
+    document.getElementById("lock3").classList.remove("fa-lock-open");
+  } else if (lock3 == true) {
+    lock3 = false;
+    document.getElementById("lock3").classList.add("fa-lock-open");
+
+    document.getElementById("lock3").classList.remove("fa-lock");
+  }
+});
+
+// locking functionality end
+
 function newidea() {
-  template1.innerHTML =
-    uiuxdesign.tarahi[Math.floor(Math.random() * uiuxdesign.tarahi.length)];
-  template2.innerHTML =
-    uiuxdesign.baraye[Math.floor(Math.random() * uiuxdesign.baraye.length)];
-  template3.innerHTML = cities[Math.floor(Math.random() * cities.length)];
+  if (lock1 == false) {
+    template1.innerHTML =
+      uiuxdesign.tarahi[Math.floor(Math.random() * uiuxdesign.tarahi.length)];
+  }
+
+  if (lock2 == false) {
+    template2.innerHTML =
+      uiuxdesign.baraye[Math.floor(Math.random() * uiuxdesign.baraye.length)];
+  }
+
+  if (lock3 == false) {
+    template3.innerHTML = cities[Math.floor(Math.random() * cities.length)];
+  }
 }
 newidea();
